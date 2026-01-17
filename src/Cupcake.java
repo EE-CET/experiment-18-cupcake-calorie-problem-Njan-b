1f551cf0ad691aedc7f01a424187021ee623b491
Njan-b <dhrishyadasph2005@gmail.com>
2026-01-17 15:57:03 +0530

    initial commit

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,19 +1,21 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Cupcake {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] calories = new int[n];
+        Integer[] calories = new Integer[n];
         for (int i = 0; i < n; i++) {
             calories[i] = sc.nextInt();
         }
 
-        Arrays.sort(calories); // Sort in ascending order
+        Arrays.sort(calories, Collections.reverseOrder()); // Sort descending
+
         long miles = 0;
         for (int i = 0; i < n; i++) {
-            miles += calories[n - 1 - i] * (1L << i); // Eat highest calorie first
+            miles += calories[i] * (1L << i); // 2^i multiplier
         }
 
         System.out.println(miles);
